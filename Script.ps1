30b159a1354fcf44560c699ca79540ab56f97cc8
Kaitrye <93823684+Kaitrye@users.noreply.github.com>
2022-09-11 12:27:19 +0300

    Update and rename Command.ps1 to Script.ps1

--- Script.ps1
+++ Script.ps1
@@ -1,4 +1,4 @@
 ﻿set-location D:\Project_LT\COLMAP\Colmap-1\COLMAP-3.7-windows-no-cuda
 .\colmap feature_extractor --project_path D:\Project_LT\car_1\car_1Extract.ini 
 .\colmap sequential_matcher --project_path D:\Project_LT\car_1\car_1Match.ini
-#.\colmap mapper --project_path D:\Project_LT\car_1\car_1Map.ini
+.\colmap mapper --project_path D:\Project_LT\car_1\car_1Map.ini
